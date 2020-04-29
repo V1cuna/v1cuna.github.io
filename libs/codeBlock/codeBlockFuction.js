fd090b8471f7072d7a76e9dbfafa601fc9e482b0
V1cuna <842730185@qq.com>
2020-04-29 12:15:47 +0800

    Site updated: 2020-04-29 12:15:36

---
+++ libs/codeBlock/codeBlockFuction.js
@@ -0,0 +1,3 @@
+// build time:Wed Apr 29 2020 11:50:28 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
---
+++ libs/codeBlock/codeShrink.js
@@ -0,0 +1,3 @@
+// build time:Wed Apr 29 2020 11:50:28 GMT+0800 (GMT+08:00)
+$(function(){var e=$('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');$(".code-area").prepend(e);$(".code-expand").on("click",function(){if($(this).parent().hasClass("code-closed")){$(this).siblings("pre").find("code").show();$(this).parent().removeClass("code-closed")}else{$(this).siblings("pre").find("code").hide();$(this).parent().addClass("code-closed")}})});
+//rebuild by neat 
