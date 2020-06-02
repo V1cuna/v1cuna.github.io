f877fbaf1df62c0522d7fa906cc80b1057b64b58
V1cuna <842730185@qq.com>
2020-06-02 17:29:29 +0800

    Site updated: 2020-06-02 17:29:17

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,5 @@
-// build time:Thu Apr 30 2020 19:03:19 GMT+0800 (GMT+08:00)
-$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
-//rebuild by neat 
+// 代码块功能依赖
+
+$(function () {
+    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
+});
--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,3 +1,16 @@
-// build time:Thu Apr 30 2020 19:03:19 GMT+0800 (GMT+08:00)
-$(function(){var e=$('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');$(".code-area").prepend(e);$(".code-expand").on("click",function(){if($(this).parent().hasClass("code-closed")){$(this).siblings("pre").find("code").show();$(this).parent().removeClass("code-closed")}else{$(this).siblings("pre").find("code").hide();$(this).parent().addClass("code-closed")}})});
-//rebuild by neat 
+// 代码块收缩
+
+$(function () {
+  var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
+
+  $('.code-area').prepend($code_expand);
+  $('.code-expand').on('click', function () {
+    if ($(this).parent().hasClass('code-closed')) {
+      $(this).siblings('pre').find('code').show();
+      $(this).parent().removeClass('code-closed');
+    } else {
+      $(this).siblings('pre').find('code').hide();
+      $(this).parent().addClass('code-closed');
+    }
+  });
+});
